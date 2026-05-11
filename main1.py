d46c413c70260c81e2058f1761d215bb4474c8bf
Roh <rohisenthil15@gmail.com>
2026-05-11 15:19:55 +1000

    feat: added other required parameters

--- main1.py
+++ main1.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI,APIRouter, File, UploadFile
-from fastapi.responses import JSONResponse 
+from fastapi import FastAPI,APIRouter, File, UploadFile, Form
+from fastapi.responses import JSONResponse, HTMLResponse
 import json
+from typing import Annotated
+import uuid
 
 
 
 
 app=FastAPI()
 router=APIRouter(prefix="/test",tags=["bootstrap_testing"])
-json_data=None
+json_data=dict()
 
-@router.post("/saveleveljson")
-async def save_json(level_file: UploadFile = File(...)):
+
+@router.post("/create")
+#ellipsis ... means that you expect data
+async def save_json(level_file: UploadFile = File(...),sandbox_id=Annotated[str,Form(...)],creator_id=Annotated[str,Form(...)]):
     global json_data
-    json_data=json.loads(level_file.file.read())
-    return JSONResponse(json_data,status_code=200)
+    json_data["game_id"]=uuid.uuid4()
+    json_data["sandbox_id"],json_data["creator_id"]=sandbox_id,creator_id
+    json_data["json_data"]=json.loads(level_file.file.read()) #converts bytes to dict
+    return JSONResponse(json_data,status_code=200) #takes the dict as single json value for output
 
 
 @router.get("/getjson")
-def get_json():
-    return JSONResponse(json_data,status_code=200) if json_data else JSONResponse(status_code=500)
+def get_json(game_id=Annotated[str,Form(...)]):
+
+    if game_id!=json_data["game_id"]:
+        return HTMLResponse(content={"Incorrect game_id"},status_code=422)
+    else:
+        return JSONResponse(json_data,status_code=200) if json_data else JSONResponse(status_code=500)
 
 app.include_router(router)
 
